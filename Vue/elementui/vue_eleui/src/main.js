aeb81cae1e79c8e20abc2824045594a3bf99969d
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-08-10 22:26:13 +0800

    vue-elementui

--- Vue/elementui/vue_eleui/src/main.js
+++ Vue/elementui/vue_eleui/src/main.js
@@ -3,11 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
+//扫描路由配置
+import router from './router'
+//导入elementUI
+import ElementUI from "element-ui"
+//导入element css
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(router)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    render: h => h(App),//ElementUI规定这样使用
 })
---
+++ Vue/elementui/vue_eleui/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Main from '../views/Main'
+import Login from '../views/Login'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/main',
+            component: Main
+        },
+    ]
+})
