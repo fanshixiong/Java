d05d74ff66ccf98459a70ade7964fa5929516a55
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-09-22 22:38:09 +0800

    Vue axios

--- Vue/Vue-Study/axios/axios_study/src/App.vue
+++ Vue/Vue-Study/axios/axios_study/src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
+    <div>{{result}}</div>
+
+    <hello_world/>
+    <HelloWorld/>
   </div>
 </template>
 <script>
+import HelloWorld from './components/HelloWorld'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
+    HelloWorld
   },
   data () {
+    return {
+      result: ''
+    }
   },
   methods: {
   },
   computed: {
+  },
+  created () {
+    axios({
+      url: 'http://123.207.32.32:8000/home/multidata'
+    }).then(res => {
+      console.log(res)
+      this.result = res
+    })
   }
 }
 </script>
---
+++ Vue/Vue-Study/axios/axios_study/src/network/request.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export function request(config) {
+  return new Promise((resolve, reject) => {
+    //创建axios实例
+    const instance = axios.create({
+      baseURL: 'http://123.207.32.32:8000/',
+      timeout: 5000
+    })
+
+    instance(config)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(err => {
+          reject(err)
+        })
+  })
+}
