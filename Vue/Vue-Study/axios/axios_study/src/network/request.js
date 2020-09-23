5c61ce75e4c1854c4e7b55bb71290c3a8cfbe28e
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-09-23 15:21:14 +0800

    Vue sxios

--- Vue/Vue-Study/axios/axios_study/src/network/request.js
+++ Vue/Vue-Study/axios/axios_study/src/network/request.js
@@ -1,19 +1,41 @@
 import axios from 'axios'
 
-export function request(config) {
-  return new Promise((resolve, reject) => {
-    //创建axios实例
-    const instance = axios.create({
-      baseURL: 'http://123.207.32.32:8000/',
-      timeout: 5000
-    })
-
-    instance(config)
-        .then(res => {
-          resolve(res)
-        })
-        .catch(err => {
-          reject(err)
-        })
+export function request (config) {
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/',
+    timeout: 5000
   })
+  // 拦截器
+  instance.interceptors.request.use(config => {
+    console.log(config)
+    return config // 为什么要return
+  }, error => {
+    console.log(error)
+  })
+  // 响应拦截
+  instance.interceptors.response.use(res => {
+    console.log(res)
+    return res.data // 为什么要return
+  }, error => {
+    console.log(error)
+  })
+  return instance(config)
+
+  /*
+  return new Promise((resolve, reject) => {
+      //创建axios实例
+      const instance = axios.create({
+        baseURL: 'http://123.207.32.32:8000/',
+        timeout: 5000
+      })
+
+      instance(config)
+          .then(res => {
+            resolve(res)
+          })
+          .catch(err => {
+            reject(err)
+          })
+    })
+    */
 }
