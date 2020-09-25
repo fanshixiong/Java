283990186d559e7785e1509dd56e2ed2487c2249
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-09-25 22:46:11 +0800

    mall navbar

--- Vue/Vue-Study/mall/supermall/src/views/home/Home.vue
+++ Vue/Vue-Study/mall/supermall/src/views/home/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+<!--      <div slot="center">购物街</div>-->
+      <template v-slot:center><div>购物街</div></template>
+    </nav-bar>
+
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
