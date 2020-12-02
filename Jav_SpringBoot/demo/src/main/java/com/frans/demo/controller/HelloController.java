a7024c9e584fcb978c4fd985b96a310698abbf61
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-02 22:56:11 +0800

    Springboot

--- Jav_SpringBoot/demo/src/main/java/com/frans/demo/controller/HelloController.java
+++ Jav_SpringBoot/demo/src/main/java/com/frans/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.frans.controller;
+package com.frans.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
