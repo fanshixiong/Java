b608bbe7286e900fc04420fbe05b4409df084b8c
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-03-09 22:59:01 +0800

    Java

--- Jav_SpringBoot/demo02-config/src/test/java/com/frans/Demo02ConfigApplicationTests.java
+++ Jav_SpringBoot/demo02-config/src/test/java/com/frans/Demo02ConfigApplicationTests.java
@@ -1,13 +1,22 @@
 package com.frans;
 
+import com.frans.pojo.Dog;
+import com.frans.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo02ConfigApplicationTests {
 
+    @Autowired
+    Dog dog;
+    @Autowired
+    Person person;
     @Test
     void contextLoads() {
+        System.out.println(dog);
+        System.out.println(person);
     }
 
 }
