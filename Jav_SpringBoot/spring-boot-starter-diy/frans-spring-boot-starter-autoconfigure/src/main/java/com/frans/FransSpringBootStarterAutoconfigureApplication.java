a235ea408574df10375e109f71bd00a0763422ca
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-03-10 22:56:57 +0800

    Jav

---
+++ Jav_SpringBoot/spring-boot-starter-diy/frans-spring-boot-starter-autoconfigure/src/main/java/com/frans/FransSpringBootStarterAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.frans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FransSpringBootStarterAutoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FransSpringBootStarterAutoconfigureApplication.class, args);
+	}
+
+}
